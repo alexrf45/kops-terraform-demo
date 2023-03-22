e7bd4f3f8fe0134571514f6fcdf92e33d7b0ba56
alexrf45 <fonalex45@gmail.com>
2023-03-21 21:43:28 -0400

    prep bucket for cluster build permissions

--- live/s3/main.tf
+++ live/s3/main.tf
@@ -37,9 +37,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
 
 resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket                  = aws_s3_bucket.terraform_state.id
-  block_public_acls       = true
+  block_public_acls       = false
   block_public_policy     = true
-  ignore_public_acls      = true
+  ignore_public_acls      = false
   restrict_public_buckets = true
 }
 
